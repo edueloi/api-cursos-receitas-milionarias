ea4e54f052aa0891ba326cbaf620ea7afa2ecd8d
Eduardo Eloi Silva Santos <eduardo.santos@comexport.com.br>
2026-01-18 17:46:10 -0300

    novo

--- server.js
+++ server.js
@@ -19,13 +19,6 @@ const PORT = process.env.PORT || 3030;
 /* =========================================
  * MIDDLEWARES
  * ========================================= */
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(204);
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
